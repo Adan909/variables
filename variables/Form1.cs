65184f4d1d08565aa8efa07c8a275cf170627809
Adan909 <jorgeot@uamv.edu.ni>
2025-09-22 07:56:11 -0600

    quedamos a medias, continuamos el miercoles

--- variables/Form1.cs
+++ variables/Form1.cs
@@ -16,5 +16,10 @@ namespace variables
         {
             InitializeComponent();
         }
+
+        private void btnVerificar_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
